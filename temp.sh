54e445e4de50c85a730f9b61ae0b219137466e10
Travis CI <travis@travis-ci.org>
2020-01-03 11:01:43 +0800

    update for voron ref,

--- temp.sh
+++ temp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone --depth=1 https://github.com/prusa3d/Original-Prusa-MINI.git _ref/prusa3d/Original-Prusa-MINI
+git clone --depth=1 https://github.com/Seamen82/VORON2-Mods.git _ref/Seamen82/VORON2-Mods
 
 python3 _util/shallow_submodule.py
 
